340a5f545e725894baec2f06bf4f89614cf9ed26
gangchenyeshi <yestsering@gmail.com>
2022-09-13 18:45:58 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+
+
+// components
+// import MyNavbar from "./components/heading/Navbar";
+// import Footer from "./components/footer/Footer";
+// import Home from "./components/pages/home/Home";
+// import Work from "./components/pages/works/Work";
+// import AboutMe from "./components/pages/about/AboutMe";
+// import ContactMe from "./components/pages/contact/ContactMe";
+// import ECommerce from "./components/pages/works/ecommerce/Ecommerce";
+// import Otizi from "./components/pages/works/otizi/Otizi";
+// import DietNutrition from "./components/pages/works/dietnutrition/DietNutrition";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yeshi
-        </a>
-      </header>
+    <div>
+      Hello React
+      {/* <BrowserRouter>
+        <MyNavbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about_me" element={<AboutMe />} />
+          <Route path="/work" element={<Work />} />
+            <Route exact path="/work/e-commerce_bottega" element={<ECommerce/>} />
+            <Route exact path="/work/otizi" element={<Otizi/>} />
+            <Route exact path="/work/diet_n_nutrition" element={<DietNutrition/>} />
+            <Route exact path="/contact_me" element={<ContactMe/>}/>
+
+          
+        </Routes>
+        
+        <Footer />
+      </BrowserRouter> */}
     </div>
   );
 }
